a471a500208c2430d98abf244d405df4887309fc
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-09-19 13:47:54 -0300

    feature/1 Fix a post tweet test
    - add a before do
    - now test is working
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

---
+++ spec/request/tweet/post_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe '::Api::V1::TweetController', type: :request do
+
+  before do
+    I18n.default_locale = :en
+    execute_actions
+  end
+
+  let(:execute_actions) {}
+
+  context 'When send a tweet' do
+    context 'When use a POST url with valid params'
+      let(:execute_actions) do
+        post '/api/v1/tweet', params: {"userId": 1, "tweet": "first tweet"}, headers: { 'ACCEPT' => 'application/json' }
+      end
+
+      it 'must be return status 201' do
+        expect(JSON.parse(response.body).status).to eq 201
+      end
+
+  end
+end
