29a31635ec6b98b174e28d00d61b82b3cda9a981
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-09-19 14:26:53 -0300

    feature/1 Fix identation
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- spec/request/tweet/post_spec.rb
+++ spec/request/tweet/post_spec.rb
@@ -11,23 +11,20 @@ describe '::Api::V1::TweetController', type: :request do
 
   context 'When send a tweet' do
     context 'When use a POST url with valid params'
-      let(:execute_actions) do
-        post '/api/v1/tweet', params: {"userId": 1, "tweet": "first tweet"}, headers: { 'ACCEPT' => 'application/json' }
-      end
+    let(:execute_actions) do
+      post '/api/v1/tweet', params: {"userId": 1, "tweet": "first tweet"}, headers: { 'ACCEPT' => 'application/json' }
+    end
 
-      it 'must be return status 201' do
-        expect(JSON.parse(response.body)['status']).to eq 201
-      end
+    it 'must be return status 201' do
+      expect(JSON.parse(response.body)['status']).to eq 201
+    end
 
-      it 'must be return user id 1' do
-        expect(JSON.parse(response.body)['data']['user_id']).to eq 1
-      end
+    it 'must be return user id 1' do
+      expect(JSON.parse(response.body)['data']['user_id']).to eq 1
+    end
 
-      it 'must be return message first tweet' do
-        expect(JSON.parse(response.body)['data']['message']).to eq 'first tweet'
-      end
-
-
-
+    it 'must be return message first tweet' do
+      expect(JSON.parse(response.body)['data']['message']).to eq 'first tweet'
+    end
   end
 end
